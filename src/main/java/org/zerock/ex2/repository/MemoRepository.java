ab19345e6e960fc5c2422695e584abe4d725c64a
KIMSUNJIN <rlatjswls91@gmail.com>
2021-07-12 23:58:14 +0900

    JpaRepository Paging, sort test

--- src/main/java/org/zerock/ex2/repository/MemoRepository.java
+++ src/main/java/org/zerock/ex2/repository/MemoRepository.java
@@ -1,7 +1,15 @@
 package org.zerock.ex2.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.ex2.entity.Memo;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    // Memo 객체의 mno 값이 70 ~ 80 사이의 객체들을 구하고 mno의 역순으로 정렬
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
 }
--- src/test/java/org/zerock/ex2/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/MemoRepositoryTests.java
@@ -3,11 +3,16 @@ package org.zerock.ex2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 import org.zerock.ex2.entity.Memo;
 import org.zerock.ex2.repository.MemoRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -63,5 +68,61 @@ public class MemoRepositoryTests {
     public void testDelete() {
         Long mno = 100L;
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault() {
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+        System.out.println("===================================");
+        System.out.println("Total Pages: "+result.getTotalPages()); //총 몇 페이지
+        System.out.println("Total Count: "+result.getTotalElements()); //전체 개수
+        System.out.println("Page Number: "+result.getNumber()); //현재 페이지 번호(0부터 시작)
+        System.out.println("Page Size: "+result.getSize()); //페이지당 데이터 개수
+        System.out.println("has next page?: "+result.hasNext()); //다음 페이지 존재 여부
+        System.out.println("is first page?: "+result.isFirst()); //시작 페이지(0) 여부
+
+        result.getContent().forEach(System.out::println);
+    }
+
+    @Test
+    public void testSort() {
+        Sort sort1 = Sort.by("mno").descending();
+        Pageable pageable = PageRequest.of(0, 10, sort1);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(System.out::println);
+    }
+
+    @Test
+    public void testSort2() {
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText");
+        Sort sortAll = sort1.and(sort2);
+
+        Pageable pageable = PageRequest.of(0, 10, sortAll);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(System.out::println);
+    }
+
+    @Test
+    public void testQueryMethods() {
+        List<Memo> result = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+        for (Memo memo : result) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testQueryMethods2() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+        for (Memo memo : result) {
+            System.out.println(memo);
+        }
     }
 }
